77792377a641f2f5d4c5f45c015fa766a93be0e0
yannik131 <yannik131@web.de>
2023-10-24 21:19:53 +0200

    Fixed off-by-one error

--- 1/yannik_1_1.c
+++ 1/yannik_1_1.c
@@ -9,6 +9,7 @@ const int EMPTY = -1;
 typedef struct SimulationState {
     int number_of_balls;
     int number_of_compartments;
+    int number_of_rows;
 } SimulationState;
 
 bool ball_goes_right() {
@@ -48,9 +49,9 @@ void init_array_with(int* array, size_t size, int value) {
     }
 }
 
-void count_and_clear_last_row(int** game_field, int* histogram, int number_of_compartments) {
-    int* last_row = game_field[number_of_compartments - 1];
-    for(int i = 0; i < number_of_compartments; ++i) {
+void count_and_clear_last_row(int** board, int* histogram, SimulationState state) {
+    int* last_row = board[state.number_of_rows - 1];
+    for(int i = 0; i < state.number_of_compartments; ++i) {
         if(last_row[i] != EMPTY) {
             ++histogram[i];
             last_row[i] = EMPTY;
@@ -63,9 +64,9 @@ void count_and_clear_last_row(int** game_field, int* histogram, int number_of_co
 Starts with the last row and works itself up.
 The last row is already assumed to be empty.
 */
-void let_balls_fall_1_row(int** game_field, int number_of_compartments) {
-    for(int i = number_of_compartments - 1; i > 0; --i) {
-        int* row_above = game_field[i - 1];
+void let_balls_fall_1_row(int** board, SimulationState state) {
+    for(int i = state.number_of_rows - 1; i > 0; --i) {
+        int* row_above = board[i - 1];
         int size_of_row_above = i + 1;
 
         // Get the index and ball number of the ball in the row above
@@ -90,19 +91,18 @@ void let_balls_fall_1_row(int** game_field, int number_of_compartments) {
         }
 
         //Assign the ball to the new index and remove it from the old
-        int* current_row = game_field[i];
+        int* current_row = board[i];
         current_row[new_index] = ball_number;
         row_above[index_of_ball_above] = EMPTY;
     }
 }
 
-
-void insert_ball_at_top(int** game_field, int number_of_balls) {
-    if(number_of_balls < 0) {
+void insert_ball_at_top(int** board, SimulationState state) {
+    if(state.number_of_balls <= 0) {
         return;
     }
     int index = ball_goes_right()? 1 : 0;
-    game_field[0][index] = number_of_balls;
+    board[0][index] = state.number_of_balls;
 }
 
 /*
@@ -116,9 +116,14 @@ void display_histogram(int* histogram, SimulationState state) {
             max = histogram[i];
         }
     }
+
+    int number_of_balls_per_X = (int)round(max / state.number_of_compartments);
+    if(number_of_balls_per_X < 1) {
+        number_of_balls_per_X = 1;
+    }
     for(int i = state.number_of_compartments; i > -1; --i) {
         for(int j = 0; j < state.number_of_compartments; ++j) {
-            if(histogram[j] > (i + 1) * max / state.number_of_compartments) {
+            if(histogram[j] >= (i + 1) * number_of_balls_per_X) {
                 printf("X ");
             }
             else {
@@ -133,26 +138,30 @@ void display_histogram(int* histogram, SimulationState state) {
         printf("\n");
     }
 
-    printf("\nX = %d balls. Underlying histogram:\n", (int)round(max / state.number_of_compartments));
+    printf("\nX = %d balls. Underlying histogram:\n", number_of_balls_per_X);
+    int sum = 0;
     for(int i = 0; i < state.number_of_compartments; ++i) {
         printf("%d ", histogram[i]);
+        sum += histogram[i];
     }
+    printf("\nSum: %d\n", sum);
 }
 
 int** create_board(SimulationState state) {
-    int** game_field = (int**)malloc(state.number_of_compartments * sizeof(int*));
-    for(size_t i = 0; i < state.number_of_compartments; ++i) {
+    int** board = (int**)malloc(state.number_of_rows * sizeof(int*));
+    for(size_t i = 0; i < state.number_of_rows; ++i) {
         size_t size = i + 2;
-        game_field[i] = (int*)malloc(size * sizeof(int));
-        init_array_with(game_field[i], size, EMPTY);
+        board[i] = (int*)malloc(size * sizeof(int));
+        init_array_with(board[i], size, EMPTY);
     }
 
-    return game_field;
+    return board;
 }
 
 /*Simulation concept:
-    Let's say there are 2 balls and 3 compartments. 
-    Every ball will have a unique number starting with 0.
+    Let's say there are 2 balls and 3 compartments. The board
+    will have 3 - 1 = 2 rows.
+    Every ball will have a unique number starting with 1.
     Every row of the board corresponds to an n+2 array, where n
     is the row number (starting with zero). It will contain a ball
     number at the ball's current position and -1 everywhere else.
@@ -164,16 +173,16 @@ int** create_board(SimulationState state) {
 */
 int* run_simulation(SimulationState state) {
     set_random_seed();
-    int** game_field = create_board(state);
+    int** board = create_board(state);
 
     int* histogram = (int*)malloc(state.number_of_compartments * sizeof(int));
     init_array_with(histogram, state.number_of_compartments, 0);
 
-    int number_of_iterations = state.number_of_balls + state.number_of_compartments + 1;
+    int number_of_iterations = state.number_of_balls + state.number_of_rows;
     for(int i = 0; i < number_of_iterations; ++i) {
-        insert_ball_at_top(game_field, state.number_of_balls);
-        let_balls_fall_1_row(game_field, state.number_of_compartments);
-        count_and_clear_last_row(game_field, histogram, state.number_of_compartments);
+        insert_ball_at_top(board, state);
+        let_balls_fall_1_row(board, state);
+        count_and_clear_last_row(board, histogram, state);
         --state.number_of_balls;
     }
 
@@ -197,6 +206,7 @@ SimulationState read_simulation_state() {
         printf("The number of compartments has to be greater than 1.\n");
         state.number_of_compartments = safely_read_integer();
     }
+    state.number_of_rows = state.number_of_compartments - 1;
 
     printf("\nRunning simulation with: \n"
            "Number of balls: %d\n"
