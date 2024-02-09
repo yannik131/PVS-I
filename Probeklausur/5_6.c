72afb4ba79ae155d68364176b159ecd972a24987
yannik131 <yannik131@web.de>
2024-02-09 11:03:01 +0100

    5_6

---
+++ Probeklausur/5_6.c
@@ -0,0 +1,11 @@
+/**
+ * 5 a) Knoten=..., Kanten=..., Grad=3, Knotenkonnektivität = Kantenkonnektivität = 2, Bisektionsbreite = 3, Durchmesser = 7
+ * 5 b) Siehe Übung
+ * 6 a)
+ * 1  2  3  4
+ * 16 7  6  5
+ * 15 8  9  10
+ * 14 13 12 11
+ * 6 b) Müsste gehen, wenn dafür g2 mehr Knoten und Kanten insgesamt hat als g1
+ * 6 c) ist mir zu hoch
+*/
